efa0c9fb17c4e622d38d78e7f52068e3dd0b258c
Saw Pyae Yadanar <sawpyae.yadanar@7peakssoftware.com>
2023-08-09 14:53:50 +0700

    first commit

---
+++ CineRater/CineRater/CineRaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CineRaterApp.swift
+//  CineRater
+//
+//  Created by Saw Pyae Yadanar on 9/8/23.
+//
+
+import SwiftUI
+
+@main
+struct CineRaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
