331b0dc5d6d822f2520dd6402e6b7e0690181940
Saw Pyae Yadanar <sawpyae.yadanar@7peakssoftware.com>
2023-12-03 15:40:54 +0700

    add fastLane

--- CineRater/CineRaterTests/CineRaterTests.swift
+++ CineRater/CineRaterTests/CineRaterTests.swift
@@ -7,35 +7,33 @@
 
 import XCTest
 @testable import CineRater
-
+@MainActor
 final class CineRaterTests: XCTestCase {
-
+    
     var sot: MovieDiscoverViewModel!
     
-    @MainActor override func setUpWithError() throws {
+    override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
         sot = .init(apiService: APIPreviewClient())
     }
-
+    
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         sot = nil
     }
-
-     func test_getTrendingData() throws {
+    
+    func test_getTrendingData() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         // Any test you write for XCTest can be annotated as throws and async.
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-         Task {
-             @MainActor in
-             sot.loadTrending()
-             XCTAssertEqual(sot.trending.count, 20)
-         }
+        sot.loadTrending()
+        debugPrint(sot.trending.count)
+         XCTAssertEqual(sot.trending.count, 20)
         
     }
-
-
-
+    
+    
+    
 }
